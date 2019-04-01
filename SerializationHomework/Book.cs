c7e97ee3ee25c74c55f6474b6f2105a7679ab4ca
DexiGG <invokedex@gmail.com>
2019-04-02 00:32:10 +0600

    SerializationHM

---
+++ SerializationHomework/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationHomework
+{
+    [Serializable]
+    class Book
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string name, int price, string author, int year)
+        {
+            Name = name; Price = price; Author = author; Year = year;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Название: {Name}" +
+                $"\nАвтор: {Author}" +
+                $"\nГод: {Year}" +
+                $"\nЦена: {Price} \n");
+        }
+    }
+}
+/*1.	Объявить в консольном приложении класс Book с полями: название, стоимость, автор, год. 
+ * Создать коллекцию элементов Book и заполнить тестовыми данными. С помощью класса BinaryFormatter сохранить 
+ * состояние приложения в двоичный файл.*/
---
+++ SerializationHomework/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book first = new Book( "Унесенные ветром", 5000, "Маргарет Митчелл", 2017 );
+            Book second = new Book( "Записки юного врача", 4000, "Михаил Булгаков", 2013 );
+            List<Book> list = new List<Book>();
+            list.Add(first); list.Add(second);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using(FileStream fs = File.Create("Books.txt"))
+            {
+                formatter.Serialize(fs, list);
+            }
+
+            using(FileStream fs = new FileStream("Books.txt", FileMode.Open))
+            {
+                List<Book> newList = (List<Book>)formatter.Deserialize(fs);
+
+                foreach (var book in newList)
+                {
+                    book.Show();
+                }
+            }
+        }
+    }
+}
+/*1.	Объявить в консольном приложении класс Book с полями: название, стоимость, автор, год. 
+ * Создать коллекцию элементов Book и заполнить тестовыми данными. С помощью класса BinaryFormatter сохранить 
+ * состояние приложения в двоичный файл.*/
+
+/*2.	На основании задания 1 восстановить состояние приложения из двоичного файла.*/
